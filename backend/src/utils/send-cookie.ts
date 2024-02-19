606792ff239bf33edc1b349333dc6a6c75a9dddd
ArjunDahal99 <dahalarjun409@gmail.com>
2024-02-19 17:40:46 +0545

    coloe

--- backend/src/utils/send-cookie.ts
+++ backend/src/utils/send-cookie.ts
@@ -1,18 +1,18 @@
-import { Response } from 'express'
-import { TokenResponseType } from '../libs/generateTokenLibs/generate-access-&-refresh-token'
-import { UserModelType } from '../types/userModelTypes/user-types'
+import { Response } from 'express';
+import { TokenResponseType } from '../libs/generateTokenLibs/generate-access-&-refresh-token';
+import { UserModelType } from '../types/userModelTypes/user-types';
 
 export const sendUserSessionCookie = (user: UserModelType, tokens: TokenResponseType, res: Response, message: string, statuscode = 200) =>
 {
-    const { accessToken, refreshToken } = tokens
+    const { accessToken, refreshToken } = tokens;
     const options = {
-        secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        //sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    }
+        maxAge: 900000, // Set the maxAge in milliseconds
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none" as "lax" | "none" | "strict" | undefined,
+        secure: process.env.NODE_ENV === "Development" ? false : true,
+    };
     res.status(statuscode)
-
         .cookie("accessToken", accessToken, options)
         .cookie("refreshToken", refreshToken, options)
-        .json({ success: true, user, accessToken, refreshToken, message })
-}
+        .json({ success: true, user, accessToken, refreshToken, message });
+};
